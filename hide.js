6fa01a06e5e56f401263c5aa53a97000f3077e1c
Adam Smith <atsmith227@gmail.com>
2015-06-26 11:43:37 -0600

    Finished Exercise

--- hide.js
+++ hide.js
@@ -4,8 +4,31 @@ $(document).ready(function(){
     var $button = $('#form-button');
     
     $button.on('click', function() {
+        // Toggle the form visibility
         $form.toggle();
-        $(this).text('Hide Form');
+        
+        // Based on current display property, change the button text
+        if ($form.css('display') === 'block') {
+            $(this).text('Hide Form'); 
+        } else {
+            $(this).text('Show Form'); 
+        }
+    });
+    
+    // When input blurs, set the paragraph element value to the input value
+    $('#input-bio').on('blur', function() {
+        var inputValue = $(this).val();
+        $('.bio-content').text(inputValue);
+    });
+    
+    $('#input-books').on('blur', function() {
+        var inputValue = $(this).val();
+        $('.books-content').text(inputValue);
+    });
+    
+    $('#input-libraries').on('blur', function() {
+        var inputValue = $(this).val();
+        $('.fav-libraries-content').text(inputValue);
     });
     
 });
